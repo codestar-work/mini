c78165ca717c24c14c7c7ff6c1d2949bbe2c12af
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-10-11 21:21:58 +0700

    Save user information to MongoDB using get method

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.listen(2000)
 
 app.get('/', showIndex)
 app.get('/register', showRegister)
+app.get('/register-user', saveNewUser)
 
 app.use(express.static('public'))
 
@@ -16,3 +17,16 @@ function showIndex(req, res) {
 function showRegister(req, res) {
     res.render('register.html')
 }
+function saveNewUser(req, res) {
+    var info = {}
+    info.name = req.query.name
+    info.email = req.query.email
+    info.password = req.query.password
+    mongo.MongoClient.connect('mongodb://127.0.0.1/minishop',
+        (error, db) => {
+            db.collection('user').insert(info)
+            res.redirect('/')
+        }
+    )
+}
+
