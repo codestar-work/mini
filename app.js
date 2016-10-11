c9fa2ff97c869939a0d4ab6a990a0071c5845234
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2016-10-11 20:59:56 +0700

    Update Main App

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var express = require('express')
 var ejs     = require('ejs')
 var mongo   = require('mongodb')
+var app     = express()
+app.engine('html', ejs.renderFile)
+app.listen(2000)
 
+app.get('/', showIndex)
+app.get('/register', showRegister)
 
+app.use(express.static('public'))
+
+function showIndex(req, res) {
+    res.render('index.html')
+}
+function showRegister(req, res) {
+    res.render('register.html')
+}
